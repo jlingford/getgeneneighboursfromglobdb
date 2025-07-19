15b82d3557054b6a27eaa369cfb4fad5f7da8d4f
jlingford <james.lingford@gmail.com>
2025-07-19 18:26:25 +1000

    minor updates to dnafreatures test script

--- dnafeaturesview.py
+++ dnafeaturesview.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
-from dna_features_viewer import BiopythonTranslator
-from dna_features_viewer import GraphicRecord
+import dna_features_viewer as dfv
+from BCBio import GFF
+# from dna_features_viewer import BiopythonTranslator
+# from dna_features_viewer import GraphicRecord
 
+# file = GFF.parse("./data/nife_test.gff")
+file = "./data/nife_test.gff"
 
-graphic_record = BiopythonTranslator().translate_record("./nife_test.gff")
+# graphic_record = dfv.BiopythonTranslator().translate_record("./data/nife_test.gff")
+graphic_record = dfv.BiopythonTranslator().translate_record(file)
 
 # graphic_record = graphic_record.GraphicRecord()
 graphic_record = graphic_record.crop((200000, 220000))
 
 ax, _ = graphic_record.plot(figure_width=20, strand_in_label_threshold=3)
 ax.figure.tight_layout()
-ax.figure.savefig("test2.png")
+ax.figure.savefig("output/test2.png")
